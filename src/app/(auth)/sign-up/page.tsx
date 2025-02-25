4a1e335f9c4c4ead9849719e7e835106a31825db
Your Name <shahiryararif@gmail.com>
2025-02-26 03:38:10 +0500

    Created Auth UI and Dotted Separator

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
+
+function SignUp() {
+    return (  
+        <div>
+            <SignUpCard />
+        </div>
+    );
+}
+
+export default SignUp;
